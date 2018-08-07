89b75448c546f1fb1961efe2e20e2416c5a17e76
Alexandre Fialho <alexandrefialhobr@gmail.com>
2018-08-07 12:26:24 -0300

    ROBO TESTE

---
+++ OScanWebAPI/Controllers/RoboController.cs
@@ -0,0 +1,24 @@
+﻿using OScanWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OScanWebAPI.Controllers
+{
+    public class RoboController : ApiController
+    {
+        private dbHammerspaceEntities db = new dbHammerspaceEntities();
+
+        [Route("api/Robo/Teste/{issue}")]
+        public void Teste(string issue)
+        {
+            db.Assinatura.Add(new Assinatura()
+            {
+                Nome = issue
+            });
+        }
+    }
+}
